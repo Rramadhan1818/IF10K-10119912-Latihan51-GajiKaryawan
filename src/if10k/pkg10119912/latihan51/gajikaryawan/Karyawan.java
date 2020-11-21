ac356523f57a8e81c5fc7dd3b0a45b7fb35380a8
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-11-21 18:52:09 +0700

    first commit

---
+++ src/if10k/pkg10119912/latihan51/gajikaryawan/Karyawan.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan51.gajikaryawan;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ */
+public class Karyawan {
+       protected String nik;
+    protected String nama;
+    protected String jabatan;
+    protected int golongan;
+
+    public String getNik() {
+        return nik;
+    }
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJabatan() {
+        return jabatan;
+    }
+
+    public void setJabatan(String jabatan) {
+        this.jabatan = jabatan;
+    }
+
+    public int getGolongan() {
+        return golongan;
+    }
+
+    public void setGolongan(int golongan) {
+        this.golongan = golongan;
+    }
+    
+}
